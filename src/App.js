f5ad656cca46236ba0059d45fda63530d36b329d
Roopesh127 <roopeshvish2006@gmmail.com>
2023-09-07 15:42:50 +0530

    new update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Board from "./components/Boards";
+
+const App = () => {
+  return (
+    <div>
+      <Board /> {" "}
+    </div>
+  );
+};
+
+export default App;
